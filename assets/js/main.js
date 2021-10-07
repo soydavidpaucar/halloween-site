34d88a8bac7db6003f18650624e134fa42bbbcf9
David Paucar <soydavidpaucar@gmail.com>
2021-10-06 21:36:50 -0500

    :children_crossing: open and close nav
    created code to open and close mobile nav

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+/*===== SHOW MENU =====*/
+const navMenu = document.getElementById('nav-menu'),
+	navToggle = document.getElementById('nav-toggle'),
+	navClose = document.getElementById('nav-close');
+
+/* Menu Show */
+if (navToggle) {
+	navToggle.addEventListener('click', () => {
+		navMenu.classList.add('show-menu');
+	});
+}
+
+/* Menu Hidden */
+if (navClose) {
+	navClose.addEventListener('click', () => {
+		navMenu.classList.remove('show-menu');
+	});
+}
+
+/*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll('.nav__link');
+
+const linkAction = () => {
+	const navMenu = document.getElementById('nav-menu');
+	// When we click on each nav__link, we remove the show-menu
+	navMenu.classList.remove('show-menu');
+};
+navLink.forEach(link => link.addEventListener('click', linkAction));
