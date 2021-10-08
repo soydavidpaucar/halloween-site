050295cabcd6df4cf896f619cf4da45b69a3b1d4
David Paucar <soydavidpaucar@gmail.com>
2021-10-07 21:04:01 -0500

    :art: background changer
    created code to change background header on scrollY axis

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,3 +36,15 @@ let homeSwiper = new Swiper('.home-swiper', {
 		clickable: true
 	}
 });
+
+/*===== CHANGE BACKGROUND HEADER =====*/
+const scrollHeader = () => {
+	const header = document.getElementById('header');
+	// When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+	if (this.scrollY >= 50) {
+		header.classList.add('scroll-header');
+	} else {
+		header.classList.remove('scroll-header');
+	}
+};
+window.addEventListener('scroll', scrollHeader);
